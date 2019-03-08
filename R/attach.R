f736b4b3e8ff5e5a47eb1d0e399d77e57c18b409
David Kahle <david.kahle@gmail.com>
2019-03-08 14:57:46 -0600

    minor updates to start messages

--- R/attach.R
+++ R/attach.R
@@ -3,11 +3,11 @@
   packageStartupMessage('  Please cite latte! See citation("latte") for details.')
   
   if (!has_4ti2()) {
-    packageStartupMessage("4ti2 was not set in .Renviron. Use set_4ti2_path() to set it.")
+    packageStartupMessage("  - 4ti2 was not set in .Renviron. Use set_4ti2_path() to set it.")
   }
   
   if (!has_latte()) {
-    packageStartupMessage("LattE was not in .Renviron. Use set_latte_path() to set it.")
+    packageStartupMessage("  - LattE was not in .Renviron. Use set_latte_path() to set it.")
   }
   
   invisible(TRUE)
