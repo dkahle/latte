ced823d1ec99e5cd966dc07fba7c30a7b812951e
David Kahle <david.kahle@gmail.com>
2019-03-20 12:06:55 -0500

    fix typo in latte doc

--- R/latte.R
+++ R/latte.R
@@ -2,7 +2,7 @@
 #'
 #' Back-end connections to LattE (\url{https://www.math.ucdavis.edu/~latte/})
 #' and 4ti2 (\url{http://www.4ti2.de/}) executables and front-end tools
-#' facilitating its use in the R ecosystem.
+#' facilitating their use in the R ecosystem.
 #'
 #' @docType package
 #' @import mpoly
